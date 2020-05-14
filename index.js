1a8bfd9b44c9af1d155b3c58c9c449b90311ca71
nisarhassan12 <syednisarhassan12@gmail.com>
2020-05-14 21:16:11 +0500

    add focus state styles for keyboard navigation.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+/* -----------------------------------------
+  Have focus outline only for keyboard users 
+ ---------------------------------------- */
+
+const handleFirstTab = (e) => {
+  if(e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing')
+
+    window.removeEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleMouseDownOnce)
+  }
+
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
+
+  window.removeEventListener('mousedown', handleMouseDownOnce)
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+window.addEventListener('keydown', handleFirstTab)
+
 const backToTopButton = document.querySelector(".back-to-top");
 let isBackToTopRendered = false;
 
